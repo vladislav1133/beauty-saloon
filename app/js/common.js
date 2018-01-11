e3f72508ac5c06c33b16e173ed429d791b1aa311
vladislav1133 <vladislav1133@ukr.net>
2018-01-11 18:03:17 +0200

    header

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,34 @@
-$(function() {
+$(function () {
 
-	// Custom JS
+    $('#my-menu').mmenu({
+        extensions: ['position-right', 'widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
+        navbar: {
+            title: '<img src="img/logo-1.svg" alt="Салон красоты">'
+        },
 
+        hooks: {
+            "openPanel:start": function () {
+                console.log('open11')
+            },
+            "openPanel:finish": function () {
+                console.log('open11')
+            },
+            "closePanel": function () {
+                console.log('close')
+            }
+        }
+    })
+
+    //var api = $('#my-menu').data('mmenu')
+
+    //console.log(api)
+
+
+    // api
+    //     .bind('opened', function () {
+    //         $('.hamburger').addClass('is-active')
+    //     })
+    //     .bind('closed', function () {
+    //         $('.hamburger').removeClass('is-active')
+    //     })
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/jQuery.mmenu/dist/jquery.mmenu.all.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
