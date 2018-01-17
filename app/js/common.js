9ada5d3d0fb9c4adba63394567b3d5b0063c1d2f
vladislav1133 <vladislv1133light@ukr.net>
2018-01-17 18:58:22 +0200

    finish carousel

--- app/js/common.js
+++ app/js/common.js
@@ -8,18 +8,74 @@ $(function () {
 
     })
 
-    var API = $('#my-menu').data( "mmenu" );
+    var API = $('#my-menu').data( "mmenu" )
 
 
 
     API.bind( "open:finish", function() {
 
             $('.hamburger').addClass('is-active')
-       ;
-    });
+
+    })
     API.bind( "close:finish", function() {
 
             $('.hamburger').removeClass('is-active')
 
-    });
+    })
+
+    $('.carousel-services').on('initialized.owl.carousel',function(){
+
+        setTimeout(function () {
+            carouselService()
+        },200)
+
+    })
+
+    $('.carousel-services').owlCarousel({
+        smartSpeed: 700,
+        nav: true,
+        dots: false,
+        navText: [
+            '<i class="fa fa-angle-double-left"></i>',
+            '<i class="fa fa-angle-double-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1100: {
+
+                items: 3
+            }
+        }
+
+    })
+
+    $('.carousel-services-content').equalHeights()
+    
+    function carouselService() {
+
+        $('.carousel-services-item').each(function () {
+            var ths = $(this)
+            var thsHeight = ths.find('.carousel-services-content').outerHeight()
+            ths.find('.carousel-services-image').css('min-height',thsHeight)
+
+        })
+    }
+
+    $('.carousel-services-composition .h3').each(function () {
+        var ths = $(this)
+        ths.html(ths.html().replace(/(\S+)\s*$/,'<span>$1</span>'))
+    })
+    //Resize window
+    function onResize() {
+        $('.carousel-services-content').equalHeights()
+        
+    }
+
+    window.onresize = function(){onResize()}
 });
--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,8 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/jQuery.mmenu/dist/jquery.mmenu.all.js',
+		'app/libs/jQuery.equalHeights/jquery.equalheights.js',
+		'app/libs/owl.carousel/dist/owl.carousel.js',
 		'app/js/common.min.js' // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
